a40b1090dfaee914bfc240f4d137bfe68e3d9246
weihua <sgwhua@shao.ac.cn>
2021-07-11 20:54:56 +0800

    patch

--- readme.md
+++
@@ -1,3 +0,0 @@
-### SunflowerGUI
-
-A control system of radio telescope. This system was used to control 4.5m telescope of UCAS named Sunflower.
--- tests/internal/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2020/11/20 16:40
-# @Author  : Menzel3
-# @Site    : 
-# @File    : __init__.py
-# @Software: PyCharm
-# @version : 0.0.1
